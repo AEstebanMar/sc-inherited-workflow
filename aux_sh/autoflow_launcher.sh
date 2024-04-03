be0765688830e5e7381d83dff2303389da49af78
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-04-03 11:24:33 +0200

    Added config_file and ref_dir variables, grepped from config. Created aux_opt and constraint variables, statically defined (will be handled by daemon flags in the future). Renamed AF_VARS to variables, standard name. Added count_transcriptome variable, passed to AutoFlow (contains reference directory). Updated AutoFlow call, now uses -L flag, aux opts for auxiliary options, -e flag for more verbose logs, -n flag for constraint and -t flag for default time provided to tasks.

--- aux_sh/autoflow_launcher.sh
+++ aux_sh/autoflow_launcher.sh
@@ -5,6 +5,10 @@ source ~soft_bio_267/initializes/init_autoflow
 project_dir=`pwd`
 autoflow_dir=$project_dir/AutoFlow
 results_dir=$project_dir/results
+config_file=$project_dir/config_daemon
+ref_dir=`grep refDir $config_file | cut -f 2 -d " "`
+aux_opt="none" ### Will be handled by daemon flags in the future
+constraint="cal" ### Will be handled by daemon flags in the future
 
 if [ "$1" == "count" ] ; then # STAGE 1 OBTAINING COUNTS FROM FASTQ FILES
     TEMPLATE=$autoflow_dir/count_template.af
@@ -34,11 +38,12 @@ while IFS= read sample; do
     total_fastq_files=$(ls $project_dir/raw_data -1 | grep -c $sample)
     number_of_lanes=$((total_fastq_files / 2))
     for (( i = 1; i <= $number_of_lanes; i++ )) ; do
-        AF_VARS=`echo "
+        variables=`echo "
         \\$sample=$sample,
-        \\$lane_num=$i
+        \\$lane_num=$i,
+        \\$count_transcriptome=$ref_dir
         " | tr -d [:space:]`
-        AutoFlow -w $TEMPLATE -V "$AF_VARS" -o $RESULTS_FOLDER/$sample #$RESOURCES
+        AutoFlow -w $TEMPLATE -V $variables -o $RESULTS_FOLDER/$sample -L $aux_opt -e -n $constraint -t 00-00:59:59
     done
     S_NUMBER=$(( S_NUMBER + 1 ))
 done < $project_dir/samples_to_process
